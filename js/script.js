1be3d2191ea5df56ebc4942bd71bc4bd00f9e831
pppws <hallo@philippschaefer.de>
2016-03-21 13:14:03 +0100

    scroll-to fix for /build pages

--- js/script.js
+++ js/script.js
@@ -13,19 +13,28 @@ $(document).ready(function(){
       }
     }
   });
-  $(".fixed").each(function() {
-		var s = $(this); 
-		var pos = s.position();                    
-		$(window).scroll(function() {
-        	var windowpos = $(window).scrollTop();
-			//s.html("Distance from top:" + pos.top + "<br />Scroll position: " + windowpos);
-	        if (windowpos >= pos.top - 190) {
-	            s.addClass("stick");
-	        } else {
-	            s.removeClass("stick"); 
-	        }
-    	});
-    });  
+  $('.fixed a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top-120;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+  $(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 700) {
+    $('.fixed').fadeIn();
+  } else {
+    $('.fixed').fadeOut();
+  }
+});
 });
 
 
